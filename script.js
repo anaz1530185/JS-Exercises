75b1b9d71b62b284c9d54887f7b65b40ae19f3ad
Anaz Bin Mannan <1530185@iub.edu.bd>
2018-05-23 03:04:34 +0000

    task 5a

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-/* JavaScript code here */
+console.log("External JS");
+
+//5a
+
+ var cart = [ 
+   {
+       name: 'Shoes',
+       price: 560,
+       quantity: 4
+   },
+   {
+       name: 'Regular Tees',
+       price: 455.50,
+       quantity: 6
+   },
+   {
+       name: 'Socks',
+       price: 65.99,
+       quantity: 2
+   }];
+
+//5a
+function addItem(newitem)
+
+{
+    cart.push(newitem);
+    console.log(cart);
+}
+var newItem={name: "Watch", price:64,quantity:1};// to add item manually
+  addItem(newItem);                             //
